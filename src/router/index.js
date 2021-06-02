80682951650b077b00cdcfe1176ba94de53f692e
Luis Sr <luisfernandocolmenarez@gmail.com>
2021-06-02 19:02:54 -0400

    Signup from added

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login'
+import Signup from '../views/Signup'
 import isAuthenticated from '../controllers/auth.ctrl'
 
 Vue.use(VueRouter)
@@ -17,6 +18,11 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: Signup
   },
   {
     path: '/logout',
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+    <div>
     <nav-bar/>
     <v-container>
         <v-alert :type="alertType" class="alert-style text-capitalize" v-model="seenAlert" dismissible absolute transition="slide-y-transition"> {{ alertMsg }}</v-alert>
@@ -14,11 +14,10 @@
             <v-col xs='12' sm='10' md='4' class="ma-0 px-0">
                 <v-card outlined tile class="rounded-r">
                     <v-form class="ma-2 px-10 py-16">
-                        <p class="primary--text font-weight-black text-center text-h4">Login</p>
+                        <p class="primary--text font-weight-black text-center text-h4">Sing Up</p>
                         <v-text-field v-model="username" :counter="10" label="Username" required></v-text-field>
                         <v-text-field v-model="password" :counter="10" label="Password" required></v-text-field>
-                        <v-btn color="primary" class="mt-12" block @click="login()">Submit</v-btn>
-                        <a href="//localhost:8080/signup">Register</a>
+                        <v-btn color="primary" class="mt-12" block>Submit</v-btn>
                     </v-form>
                 </v-card>
             </v-col>
@@ -28,14 +27,14 @@
 </template>
 
 <script>
-    import NavBar from '../components/navBar'
+import NavBar from '../components/navBar'
 
-    export default {
-        name: 'Login',
-        components: {
-            NavBar
-        },
-        data() {
+export default {
+    name: 'singup',
+    components: {
+        NavBar
+    },
+    data() {
             return {
                 username: '',
                 password: '',
@@ -44,43 +43,5 @@
                 seenAlert: false
             }
         },
-        methods: {
-            login() {
-                this.axios.post('//localhost:3000/users/login', {username: this.username, password: this.password})
-                .then((req)=> {
-                    sessionStorage.jwt = req.data.token
-                    sessionStorage.user = req.data.user
-                    this.alertMsg = 'Bienvenido ' + req.data.user
-                    this.alertType = 'success'
-                    this.seenAlert = true
-                    this.userLogged = req.data.user
-                    setTimeout(()=> this.$router.push('/'), 2000)
-                    
-                }).catch((err)=>{
-                    this.alertMsg = err.response.data
-                    this.alertType = 'error'
-                    this.seenAlert = true
-                    console.log(err.response.data)
-                })
-            },
-        },
-        watch: {
-            alertMsg() {
-                setTimeout(()=> {
-                    this.seenAlert = false
-                }, 3000)
-            }
-        },
-    }
-</script>
-
-<style lang="scss" scoped>
-.alert-style {
-    position:fixed;
-    top: 0;
-    z-index:100;
-    width: 90%;
-    margin: auto;
 }
-
-</style>
+</script>
